38b8815f60272c41b58f4afe9272c87fa2f2e94d
Aryan A Kumar <98894571+aryank34@users.noreply.github.com>
2023-12-16 01:14:30 +0530

    Delete files directory

--- files/src/modules/notes/context/note-context.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export const NoteContext = React.createContext(
-    {
-        notes:[],
-        total:0,
-        getNotes:function(){},
-        addSingleNote:function(note){}
-    });
--- files/src/shared/app/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import noteReducer from '../../modules/notes/redux/note-slice';
-
-export const store = configureStore({
-    reducer:{
-        noteReducer
-    }
-})
